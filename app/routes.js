e69e4c8cce1dbd2356a48da48e8302546901b7ac
Stu Bamforth <stuart.bamforth@engineering.digital.dwp.gov.uk>
2021-01-15 10:56:56 +0000

    First push of v06

--- app/routes.js
+++ app/routes.js
@@ -213,3 +213,116 @@ router.post('/employer/v05/job-add-3b-locally', function (req, res) {
     res.redirect('/employer/v05/job-add-3c')
   }
 });
+
+
+
+////// v05 //////////////////////////////////
+
+
+router.post('/employer/v06/manage/vacancies', function (req, res) {
+  const editChoice = req.session.data['manage-application']
+
+  if (editChoice === 'application-cancel') {
+    res.redirect('/employer/v06/manage/cancel-application-check')
+  } else if (editChoice === 'application-pause') {
+    res.redirect('/employer/v06/manage/pause-application-check')
+  }
+});
+
+
+router.post('/employer/v06/manage/cancel-application-check', function (req, res) {
+  const editChoice = req.session.data['cancel-application']
+
+  if (editChoice === 'yes') {
+    res.redirect('/employer/v06/manage/cancel-application-confirm')
+  } else if (editChoice === 'no') {
+    res.redirect('/employer/v06/manage/pause-application-no-go-back')
+  }
+});
+
+
+router.post('/employer/v06/manage/pause-application-check', function (req, res) {
+  const editChoice = req.session.data['pause-application']
+
+  if (editChoice === 'yes') {
+    res.redirect('/employer/v06/manage/pause-application-confirm')
+  } else if (editChoice === 'no') {
+    res.redirect('/employer/v06/manage/pause-application-no-go-back')
+  }
+});
+
+
+router.post('/employer/v06/manage/job-detail', function (req, res) {
+  const editChoice = req.session.data['manage-role']
+
+  if (editChoice === 'edit-role') {
+    res.redirect('/employer/v06/manage/edit-job-detail')
+  } else if (editChoice === 'pause-role') {
+    res.redirect('/employer/v06/manage/pause-role-check')
+  } else if (editChoice === 'cancel-role') {
+    res.redirect('/employer/v06/manage/cancel-role-check')
+  } else if (editChoice === 'place-referral') {
+    res.redirect('/employer/v06/manage/referrals-place-in-role')
+  } else if (editChoice === 'closing-date') {
+    res.redirect('/employer/v06/manage/closing-date')
+  } else if (editChoice === 'referral-limit') {
+    res.redirect('/employer/v06/manage/referral-limit')
+  } else if (editChoice === 'change-vacancies') {
+    res.redirect('/employer/v06/manage/vacancies')
+  }
+});
+
+
+router.post('/employer/v06/manage/cancel-role-check', function (req, res) {
+  const editChoice = req.session.data['cancel-role']
+
+  if (editChoice === 'yes') {
+    res.redirect('/employer/v06/manage/cancel-role-confirm')
+  } else if (editChoice === 'no') {
+    res.redirect('/employer/v06/manage/job-detail')
+  }
+});
+
+
+router.post('/employer/v06/manage/pause-role-check', function (req, res) {
+  const editChoice = req.session.data['pause-role']
+
+  if (editChoice === 'yes') {
+    res.redirect('/employer/v06/manage/pause-role-confirm')
+  } else if (editChoice === 'no') {
+    res.redirect('/employer/v06/manage/job-detail')
+  }
+});
+
+router.post('/employer/v06/job-add-3a', function (req, res) {
+  const editChoice = req.session.data['remote-role']
+
+  if (editChoice === 'yes') {
+    res.redirect('/employer/v06/job-add-4')
+  } else if (editChoice === 'part') {
+    res.redirect('/employer/v06/job-add-3b-locally')
+  } else if (editChoice === 'not') {
+    res.redirect('/employer/v06/job-add-3b')
+  }
+});
+
+router.post('/employer/v06/job-add-3b', function (req, res) {
+  const editChoice = req.session.data['multiple-locations']
+
+  if (editChoice === 'multiple') {
+    res.redirect('/employer/v06/job-add-3d')
+  } else if (editChoice === 'one-location') {
+    res.redirect('/employer/v06/job-add-3c')
+  }
+});
+
+router.post('/employer/v06/job-add-3b-locally', function (req, res) {
+  const editChoice = req.session.data['multiple-locations']
+
+  if (editChoice === 'multiple') {
+    res.redirect('/employer/v06/job-add-3d')
+  } else if (editChoice === 'one-location') {
+    res.redirect('/employer/v06/job-add-3c')
+  }
+});
+
